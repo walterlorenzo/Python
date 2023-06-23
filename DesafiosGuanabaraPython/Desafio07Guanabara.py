b2f6feeff47567f92347b60af22c2322447909a7
Walter <70152476+walterlorenzo@users.noreply.github.com>
2023-06-23 12:41:24 -0300

    Add files via upload

---
+++ DesafiosGuanabaraPython/Desafio07Guanabara.py
@@ -0,0 +1,4 @@
+# Desenvolva um programa que leia as duas notas de um aluno. calcule e mostre sua média #
+primeira_nota = float(input('Digite a primeira nota do aluno: '))
+segunda_nota = float(input('Digite a segunda nota do aluno: '))
+print('A média da nota é: {}'.format((primeira_nota + segunda_nota)/2))
